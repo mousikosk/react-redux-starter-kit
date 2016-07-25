7ddb73dd6c8d95a2e8b70ad56aa6d66700eb5bb2
Kostis Mousikos <kmousikos@gmail.com>
2016-07-25 12:13:13 +0300

    Zen component

---
+++ tests/routes/Zen/index.spec.js
@@ -0,0 +1,18 @@
+import ZenRoute from 'routes/Zen'
+
+describe('(Route) Zen', () => {
+  let _route
+
+  beforeEach(() => {
+    _route = ZenRoute({})
+  })
+
+  it('Should return a route configuration object', () => {
+    expect(typeof(_route)).to.equal('object')
+  })
+
+  it('Configuration should contain path `zen`', () => {
+    expect(_route.path).to.equal('zen')
+  })
+
+})
